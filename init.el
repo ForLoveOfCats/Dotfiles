0712dfb564fa8b4978817f8a92b135324b33d1b1
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-07-18 17:49:09 -0400

    Updated the omnisharp server binary

--- init.el
+++ init.el
@@ -715,7 +715,7 @@
 
 ;;C# stuff
 (add-hook 'csharp-mode-hook 'omnisharp-mode)
-(setq omnisharp-server-executable-path "~/.emacs.d/.cache/omnisharp/server/v1.32.19/run")
+(setq omnisharp-server-executable-path "~/.emacs.d/.cache/omnisharp/server/v1.34.0/run")
 (eval-after-load
  'company
  '(add-to-list 'company-backends 'company-omnisharp))
